86edca1db8cb363b2747c5d7af59b66e46683649
Viktor590 <resident1081994@gmail.com>
2022-02-11 17:23:50 +0300

    make part JS functional

--- src/js/modules/timer.js
+++ src/js/modules/timer.js
@@ -3,19 +3,26 @@ const timer = () => {
   const minuts = document.querySelector('.minuts');
   const second = document.querySelector('.second');
   const buttons = document.querySelectorAll('.btn');
+  const results = document.querySelector('.list')
+
 
   buttons.forEach(el => {
     el.addEventListener('click', (e) => {
       if (e.target.classList.contains('start')) startTimer()
-      if (e.target.classList.contains('start')) startTimer()
+      if (e.target.classList.contains('stop')) stopTimer()
+      if (e.target.classList.contains('pause')) pauseTimer()
+      if (e.target.classList.contains('lap')) lap()
     })
   })
 
   let seconds = 0;
   let time;
+  const arr = [];
+
 
   function startTimer() {
-    clearInterval(time);
+    clearTimeout(time);
+
     time = setInterval(() => {
       seconds += 10;
       let startDate = new Date(seconds);
@@ -41,15 +48,42 @@ const timer = () => {
   }
 
   function stopTimer() {
-
+    clearTimeout(time)
+    seconds = 0;
+    hour.textContent = '00';
+    minuts.textContent = '00';
+    second.textContent = '00';
   }
+
+
 
   function lap() {
+    const hourLap = hour.textContent;
+    const minutsLap = minuts.textContent;
+    const secondLap = second.textContent;
+    const element = document.createElement('li');
+    const indexLap = arr.length;
+    element.classList.add('result');
+    element.innerHTML = `
+             <div class="result-top">
+            <h3 class="result-title">
+              Lap ${indexLap}
+            </h3>
+            <button class="result-delete">Delete</button>
+          </div>
+          <h2 class="result-time">
+            ${hourLap}h:${minutsLap}m:${secondLap}s
+          </h2>
+        `;
+    results.append(element)
+    arr.push(element)
+    console.log(arr);
 
   }
 
-  function pauseTimer() {
 
+  function pauseTimer() {
+    clearTimeout(time);
   }
 
   function clearInterval() {
